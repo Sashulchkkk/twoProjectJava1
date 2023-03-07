4df298987e3d697beeb67a1136e8fd8b4b5c5835
Neto test <sashadatskaya@rambler.ru>
2023-03-07 20:09:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticPrice = 46500;
+        int ticMile = 63;
+
+        int sumMile = ticPrice/ticMile;
+
+        System.out.println("Количество миль - " + sumMile);
+
+    }
+}
